364ac79fee42d598615b9f6385bb8bf650ffb77f
David Ross <6578861+djrrb@users.noreply.github.com>
2023-04-14 13:13:18 -0400

    more comments

--- session-2/code/16-gridWithTranslate.py
+++ session-2/code/16-gridWithTranslate.py
@@ -1,18 +1,30 @@
+# define some constants
 myRowCount = 10
 myColCount = 10
 
+# calculate the cell width and height
 myCellWidth = width()/myColCount
 myCellHeight = height()/myRowCount
 
 print(myCellWidth, myCellHeight)
-#newPage(5000, 1000)
+
+# this time we will always draw the shape at (0, 0)
+# but we will use translate to move the location of (0, 0) as we go
+
+# loop through the rows
 for myRowNumber in range(myRowCount):
+    # we want to be able to return to the left of the screen
+    # so save the state
     with savedState():
+        # loop through the columns
         for myColNumber in range(myColCount):
+            # draw our shape at (0, 0)
             oval(0, 0, myCellWidth, myCellHeight)
+            # move us to the right
             translate(myCellWidth, 0)
             # this runs once per column
         # this ends the column loop
     # this exits the saved state
     # and we undo all of that rightward movement
+    # now move us up to the next row
     translate(0, myCellHeight)
--- session-2/code/17-patternWithTranslate.py
+++ session-2/code/17-patternWithTranslate.py
@@ -4,29 +4,44 @@ myColCount = 10
 myCellWidth = width()/myColCount
 myCellHeight = height()/myRowCount
 
+# make a custom function with some stuff
 def myPatternElement():
+    # let’s undo all canvas transformations
+    # within the function with a savedState
+    # this way the function is nondestructive
     with savedState():
         rect(0, 0, myCellWidth/2, myCellHeight/2)
         fill(1, 0, 0)
         #polygon((20, 20), (50, 50), (-40, 30))
-        oval(myCellWidth/2, myCellHeight/2, myCellWidth, myCellHeight)
-
-#def myPatternElement():
-#    image('myPatternElement.pdf', (0, 0))
+        oval(
+            myCellWidth/3, 
+            myCellHeight/3, 
+            myCellWidth/2, 
+            myCellHeight/2
+        )
+        oval(
+            myCellWidth*.75, 
+            myCellHeight*.75, 
+            myCellWidth/4, 
+            myCellHeight/4
+        )
 
 print(myCellWidth, myCellHeight)
-#newPage(5000, 1000)
+
+# make a grid
 for myRowNumber in range(myRowCount):
     with savedState():
         for myColNumber in range(myColCount):
+            # rather than drawing a shape
+            # call our special function
             myPatternElement()
+            # move to the right
             translate(myCellWidth, 0)
     
-            myCellWidth -= 5
+
             # this runs once per column
         # this ends the column loop
     # this exits the saved state
     # and we undo all of that rightward movement
     translate(0, myCellHeight)
     
-saveImage('blah.svg')
